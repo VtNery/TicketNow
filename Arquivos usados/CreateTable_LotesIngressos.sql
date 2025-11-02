388f5a51abbd5f8bc54878aa6e8bac02fea8e60b
Vitor Nery <88716040+VtNery@users.noreply.github.com>
2025-11-02 12:19:28 -0300

    Delete Arquivos usados directory

--- Arquivos usados/CreateTable_LotesIngressos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE LotesIngressos (
-    ID INT PRIMARY KEY,
-    nome VARCHAR(50),
-    preco DECIMAL(10, 2),
-    eventoID INT,
-    ativo BIT DEFAULT 1 -- Adiciona a coluna "ativo" com valor padrão 1
-);
